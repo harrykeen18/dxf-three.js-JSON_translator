b4b762971c57c99c555af6d3379b11d830cf3faf
Harry <harry@opendesk.cc>
2016-04-08 16:17:03 +0100

    reader: added overwrite three.txt to reader

--- dxf-reader/reader.py
+++ dxf-reader/reader.py
@@ -1,16 +1,52 @@
+def end_three():
+	file = open("three.txt",'a')
+	file.write('\ngeometry = new THREE.ShapeGeometry( shape );\n')
+	file.write('geometry = shape.extrude( { amount: -thickness, bevelEnabled: false } );\n')
+	file.write('mesh = new THREE.Mesh( geometry, material );\n')
+	file.write('mesh.position.set( 0, 0, 0 );\n')  # mesh position is based on the origin at 0,0
+	file.write('scene.add( mesh );\n\n')
+
+
+def start_three():
+
+	if 'no_feat' not in locals():
+		no_feat = 0
+
+	no_feat += 1
+
+	file = open("three.txt",'a')
+	file.write('// asset ' + str(no_feat) + '\n\n')
+	file.write('shape = new THREE.Shape();\n\n')
+
+
+def write_three(x, y, b):
+
+	file = open("three.txt",'a')
+	file.write('shape.moveTo( ' + str(x) + ', ' + str(y) + ');\n')
+
+	# file.write(str(x) + ' ' + str(y) + ' ' + str(b) + '\n')
 
 
 def import_scan():
+	
+	# overwrite old three.txt file
+	file = open("three.txt",'w')
+	file.close
 
-	import_list = open("test.dxf").readlines()
-
+	import_list = open("real.dxf").readlines()
+	print len(import_list)
+	# remove all \n from strings
 	for n, i in enumerate(import_list):
 		if "\n" in i:
 			import_list[n] = i.replace('\n','')
 
+	# start scaning list for lines and stuff.
 	for n, i in enumerate(import_list):
 
 		if "POLYLINE" in i:
+
+			# write first lines for a new part
+			start_three()
 
 			# find index of next 'seqend' to find end of polyline feature
 			p_end = 0
@@ -34,6 +70,7 @@ def import_scan():
 					v_end = 0
 					v_add = 1
 					v_boo = False
+
 					while v_boo == False:
 						if "VERTEX" in import_list[polyline_ind + v_add] or "SEQEND" in import_list[polyline_ind + v_add]:
 							v_end = polyline_ind + v_add
@@ -43,16 +80,19 @@ def import_scan():
 					for v in range(polyline_ind, v_end):
 						if "10" in import_list[v] and \
 							"20" in import_list[v + 2] and \
-							"30" in import_list[v + 4] and \
-							"42" in import_list[v + 6]:
+							"30" in import_list[v + 4]: # and \
+							#"42" in import_list[v + 6]:
 
 							x = float(import_list[v + 1])
 							y = float(import_list[v + 3])
-							bulge = float(import_list[v + 7])
+							#bulge = float(import_list[v + 7])
+							bulge = 0.0
 
 							print x
 							print y
 							print bulge
+
+							write_three(x, y, bulge)
 						
 						# if "10" in import_list[v] and "20" in import_list[v + 2]:
 						# 	print "10 " + import_list[v + 1]
@@ -60,6 +100,10 @@ def import_scan():
 						# 	print "20 " + import_list[v + 1]
 						# if "42" in import_list[v] and "0" in import_list[v + 2]:
 						#	print "42 " + import_list[v + 1]
+
+
+			# write first lines for a new part
+			end_three()
 
 if __name__ == '__main__':
 	import_scan()
